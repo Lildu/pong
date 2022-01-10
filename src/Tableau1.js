44007d3b19154eaa713d8a33a3f8bb92e340758c
Lildu <lildci83@gmail.com>
2022-01-10 14:04:17 +0100

    dqs

---
+++ src/Tableau1.js
@@ -0,0 +1,19 @@
+/**
+ * ALGO: ceci est une classe...
+ * Vous verrez ça plus tard en détail avec Rémi, pour l'instant on n'a pas trop besoin de savoir à quoi ça sert.
+ */
+let timeline;
+class Tableau1 extends Phaser.Scene{
+    /**
+     * Précharge les assets
+     */
+
+    preload(){
+    }
+    create() {
+
+
+    }
+    update(){
+    }
+}
