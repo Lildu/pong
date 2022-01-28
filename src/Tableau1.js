a99968f6379900ebc7e395d97566e25abe938046
Lildu <lildci83@gmail.com>
2022-01-28 22:02:40 +0100

    opti son

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -16,8 +16,8 @@ class Tableau1 extends Phaser.Scene{
         this.load.image('leafemit','assets/particles/leafemit.png');
 
         this.load.audio('son','assets/Audio/seasound.mp3');
-        this.load.audio('son1','assets/Audio/son1.mp3');
-        this.load.audio('son2','assets/Audio/son2.mp3');
+        this.load.audio('son1','assets/Audio/son1.wav');
+        this.load.audio('son2','assets/Audio/son2.wav');
 
 
     }
